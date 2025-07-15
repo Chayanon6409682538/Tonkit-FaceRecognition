c4f4967beb32d10f0dc8ec506da592cd01895061
chayanon <chayanon.kha@dome.tu.ac.th>
2025-07-15 11:16:37 +0700

    add Readme and Android final version

--- FaceRecognitionImages/openCV/build/intermediates/cxx/Debug/6p4y7726/logs/x86/configure_command.bat
+++ FaceRecognitionImages/openCV/build/intermediates/cxx/Debug/6p4y7726/logs/x86/configure_command.bat
@@ -1,6 +1,6 @@
 @echo off
 "C:\\Users\\HP\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
-  "-HD:\\work space\\tonkit\\FaceRecognitionImages\\openCV\\libcxx_helper" ^
+  "-HD:\\work space\\tonkit\\git\\Tonkit-FaceRecognition\\FaceRecognitionImages\\openCV\\libcxx_helper" ^
   "-DCMAKE_SYSTEM_NAME=Android" ^
   "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
   "-DCMAKE_SYSTEM_VERSION=21" ^
@@ -11,9 +11,9 @@
   "-DCMAKE_ANDROID_NDK=C:\\Users\\HP\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973" ^
   "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\HP\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973\\build\\cmake\\android.toolchain.cmake" ^
   "-DCMAKE_MAKE_PROGRAM=C:\\Users\\HP\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=D:\\work space\\tonkit\\FaceRecognitionImages\\openCV\\build\\intermediates\\cxx\\Debug\\6p4y7726\\obj\\x86" ^
-  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=D:\\work space\\tonkit\\FaceRecognitionImages\\openCV\\build\\intermediates\\cxx\\Debug\\6p4y7726\\obj\\x86" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=D:\\work space\\tonkit\\git\\Tonkit-FaceRecognition\\FaceRecognitionImages\\openCV\\build\\intermediates\\cxx\\Debug\\6p4y7726\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=D:\\work space\\tonkit\\git\\Tonkit-FaceRecognition\\FaceRecognitionImages\\openCV\\build\\intermediates\\cxx\\Debug\\6p4y7726\\obj\\x86" ^
   "-DCMAKE_BUILD_TYPE=Debug" ^
-  "-BD:\\work space\\tonkit\\FaceRecognitionImages\\openCV\\.cxx\\Debug\\6p4y7726\\x86" ^
+  "-BD:\\work space\\tonkit\\git\\Tonkit-FaceRecognition\\FaceRecognitionImages\\openCV\\.cxx\\Debug\\6p4y7726\\x86" ^
   -GNinja ^
   "-DANDROID_STL=c++_shared"
---
+++ FaceRecognitionImages_finalVersion/openCV/build/intermediates/cxx/Debug/6p4y7726/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\HP\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\work space\\tonkit\\FaceRecognitionImages\\openCV\\.cxx\\Debug\\6p4y7726\\x86_64" ^
+  opencv_jni_shared
