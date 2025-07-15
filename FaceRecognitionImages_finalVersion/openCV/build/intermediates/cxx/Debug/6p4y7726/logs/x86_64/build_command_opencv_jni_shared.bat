bc5b3b65532f8669b46fb937939f490267874f3e
chayanon <chayanon.kha@dome.tu.ac.th>
2025-07-15 12:07:23 +0700

    add file final version

--- FaceRecognitionImages_finalVersion/openCV/build/intermediates/cxx/Debug/6p4y7726/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\HP\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\work space\\tonkit\\FaceRecognitionImages\\openCV\\.cxx\\Debug\\6p4y7726\\x86_64" ^
-  opencv_jni_shared
